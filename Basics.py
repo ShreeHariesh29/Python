66d76dde0ab0c5a50f36db8a4001da2b1e748be9
ShreeHariesh <shreehariesh29@gmail.com>
2024-01-28 13:01:11 +0530

    new

---
+++ Basics.py
@@ -0,0 +1,25 @@
+a = int(input("a="))
+b = int(input("name ="))
+# c = a*b
+# print(c)
+
+print("1- Addition \n2- Sub \n3- mul")
+
+input = int(input("enter which option ="))
+
+if input == 1:
+    c = a+b
+    print(c)
+elif input == 2:
+    c = a-b
+    print(c)
+else:
+    c = a*b
+    print(c)
+
+# if (c>20 and c<30):
+#     print("grater than 20")
+# elif c>30 :
+#     print("grater than 30")
+# else:
+#     print("leser")
